3a65005519ff54c3ed79daacc61f60fa68362935
Familia Gomes <Familia Gomes@192.168.1.5>
2018-02-08 20:42:30 -0200

    CRUD - D DELETE

--- routes/person.js
+++ routes/person.js
@@ -38,6 +38,7 @@ router.get('/:id', function(req, res){
 module.exports = router;
 
 //post.create
+// POST -> http://localhost:3000/person
 router.post('/', function(req, res){
     Person.create({
         name: {
@@ -54,6 +55,7 @@ router.post('/', function(req, res){
 });
 
 //post.save
+// POST -> http://localhost:3000/person/save
 router.post('/save', function(req, res){
     var person = new Person({
         name: {
@@ -72,6 +74,7 @@ router.post('/save', function(req, res){
 });
 
 //post.bulk_create (insertMany)
+// POST -> http://localhost:3000/person/bulk
 router.post('/bulk',function(req, res){
     var arr = [{
         name: {
@@ -102,6 +105,7 @@ router.post('/bulk',function(req, res){
 });
 
 //put.update
+// PUT -> http://localhost:3000/person/5a7a410ae5de7b0de8c3de00
 router.put('/:id',function(req, res){
     Person.update({
         _id: req.params.id
@@ -119,6 +123,7 @@ router.put('/:id',function(req, res){
 });
 
 //put.findOneAndUpdate (só atualiza os itens setados)
+//PUT -> http://localhost:3000/person/update/5a7cc6535f31cc1a8cb1ee30
 router.put('/update/:id',function(req, res){
     Person.findOneAndUpdate({
         _id: req.params.id
@@ -132,5 +137,31 @@ router.put('/update/:id',function(req, res){
             return;
         }
         res.send(person);
+    });
+});
+
+//delete.remove
+// DELETE -> http://localhost:3000/person/5a7cc6535f31cc1a8cb1ee30
+router.delete('/:id',function(req, res){
+    Person.remove({
+        _id: req.params.id
+    },function(err){
+        if(err){
+            return;
+        }
+        res.send(req.params.id);
+    });
+});
+
+//delete.findOneAndRemove
+// DELETE -> http://localhost:3000/person/delete/5a7cc6535f31cc1a8cb1ee30
+router.delete('/delete/:id',function(req, res){
+    Person.findOneAndRemove({
+        _id: req.params.id
+    },function(err){
+        if(err){
+            return;
+        }
+        res.send(req.params.id);
     });
 });
