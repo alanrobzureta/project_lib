3b5061bfacee2290497a2d63ed3bb8faea5614c3
Familia Gomes <Familia Gomes@192.168.1.5>
2018-02-08 19:40:24 -0200

    CRUD - R Read

--- routes/person.js
+++ routes/person.js
@@ -2,8 +2,37 @@ var express = require('express');
 var router = express.Router();
 var Person = require('../models/Person');
 
+//find
 router.get('/', function(req, res){
-    res.send('Hello');
+    Person.find({},function(err, people){
+            if(err){
+                return;
+            }
+            res.send(people);
+	});
+});
+
+//findOne
+router.get('/:id', function(req, res){
+    Person.findOne({
+        //_id: req.params.id
+        'name.firstname': req.params.id
+    },function(err, people){
+            if(err){
+                return;
+            }
+            res.send(people);
+	});
+});
+
+//findById
+router.get('/:id', function(req, res){
+    Person.findById(req.params.id,function(err, people){
+            if(err){
+                return;
+            }
+            res.send(people);
+	});
 });
 
 module.exports = router;
